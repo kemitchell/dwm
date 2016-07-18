95770474e28d9104a336acb909756c99a2dd5dc8
Kyle E. Mitchell <kyle@kemitchell.com>
2016-07-18 15:38:40 -0700

    Float xsane

--- config.h
+++ config.h
@@ -24,8 +24,9 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 }
+	/* class                      instance    title       tags mask     isfloating   monitor */
+	{ "Gimp",                     NULL,       NULL,       0,            1,           -1 },
+	{ "xsane",                    NULL,       NULL,       0,            1,           -1 }
 };
 
 /* layout(s) */
